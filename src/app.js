efc94db071cbdc3fd2783441891d7cafd9a9d26c
magda chikhradz <magda292005@gmail.com>
2026-06-22 17:58:40 +0400

    create simple classes

--- src/app.js
+++ src/app.js
@@ -2,19 +2,53 @@ class Plane {
   speed;
   material;
 
-  constructor(speed, material) {
-    this.speed = speed;
-    this.material = material;
+  constructor(s, m) {
+    this.speed = s;
+    this.material = m;
+  }
+  fly() {
+    // console.log(`this plane is made of ${this.material}`); //1
+    if (this.material === "iron") {
+      this.speed = this.speed + 500;
+    } else {
+      this.speed = this.speed + 1000;
+    }
+    console.log(this.speed);
   }
 }
 
 // const date = new Date();
 
-const plane1 = new Plane(500, "iron");
+const plane1 = new Plane(322, "iron");
 console.log(plane1.material);
 
-const plane2 = new Plane(600, "copper");
-console.log(plane2);
+const plane2 = new Plane(0, "copper");
+console.log(plane2.fly());
 
 const plane3 = new Plane(7800, "lava");
-console.log(plane3);
+console.log(plane3.fly());
+
+plane1.fly(); //1
+
+class Person {
+  name;
+  age;
+  constructor(n, a) {
+    this.name = n;
+    this.age = a;
+  }
+
+  user() {
+    console.log(`hello my name is ${this.name} and i am ${this.age} years old`);
+  }
+}
+
+const person1 = new Person("leila", 40);
+const person2 = new Person("natia", 23);
+const person3 = new Person("magda", 20);
+const person4 = new Person("anano", 37);
+
+console.log(person1.user());
+console.log(person2.user());
+console.log(person3.user());
+console.log(person4.user());
